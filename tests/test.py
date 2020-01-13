3393b951dcf006b48ae6652ced281f3778211221
Saroj Paudyal <Sarojpaudyal123@gmail.com>
2020-01-13 19:57:24 +0545

    cli

---
+++ tests/test.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+
+
+from todos.models import Todo
+
+
+class AnimalTestCase(TestCase):
+
+    def setUp(self):
+        Todo.objects.all()
+
+    def test_animals_can_speak(self):
+        self.assertEqual(5, 5)
+        self.assertEqual(3, 3)
