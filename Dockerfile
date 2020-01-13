b3177fe9a977a8931b60bb772ebba7bfe763d3ba
Saroj Paudyal <Sarojpaudyal123@gmail.com>
2020-01-13 23:07:38 +0545

    ksdfs

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
 FROM python:latest
 ENV PYTHONUNBUFFERED 1
-ENV PORT=80
-ENV SECRET_KEY=dlkfjdskljflkj
-ENV DEBUG=$DEBUG
 RUN mkdir /src
 WORKDIR /src
 COPY requirements.txt /src/
--- todos/views.py
+++ todos/views.py
@@ -8,7 +8,7 @@ def index(request):
     todos = list(filter(lambda todo : todo.containsTag(searchQuery), Todo.objects.all())) if searchQuery else Todo.objects.all()
     template = loader.get_template('todos/todos.html')
     content = {
-        'title' : 'This is a huge success for me LOL!',
+        'title' : 'Todos by Users',
         'todos' : todos,
         'searchQuery' : request.POST.get('searchQuery', False),
         'hasResults' : True if (len(todos) > 0) else False
